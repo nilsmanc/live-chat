e3c3763d135da75b7faab5974ea94e6b960101bd
nilsmanc <etereaiberia@gmail.com>
2023-09-09 08:21:11 +0300

    client init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
