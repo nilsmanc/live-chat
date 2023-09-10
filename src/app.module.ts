7293a95be5a7a9b9ca31b733e8693bdf26b6d95f
nilsmanc <etereaiberia@gmail.com>
2023-09-10 11:00:13 +0300

    layout render endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app/app.gateway';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppService, AppGateway, PrismaService],
 })
 export class AppModule {}
